eda8baddc21e9efa7cc14372ad0f16a3f19c00ef
Stevan Kapicic <s.kapicic@vegaitsourcing.rs>
2020-04-25 13:25:13 +0200

    add version to api route

--- ContactTracingApi/ContactTracingApi/Controllers/DiagnosisController.cs
+++ ContactTracingApi/ContactTracingApi/Controllers/DiagnosisController.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace ContactTracingApi.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     public class DiagnosisController : ControllerBase
     {
 
